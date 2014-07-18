e680dca28bd74259b387ad7a5048bbdcebf69aee
Alex Cornejo <acornejo@gmail.com>
2014-07-18 13:30:47 -0400

    add create debian package file

---
+++ create_deb.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# debian folder created by dh_make --createorig --single -c mit -e acornejo@gmail.com
+
+APPNAME=kilogui
+VERSION=1.0
+
+tar czvf ../${APPNAME}_${VERSION}.orig.tar.gz . --exclude .git --exclude debian --transform="s,^\.,${APPNAME}-${VERSION},"
+# dpkg-buildpackage
